ccaafa514bec61534a02bbbb17f26717edfbcbf9
daulet_tn <daulettanirberdi94@gmail.com>
2025-01-24 20:44:18 +0500

    ex4

---
+++ lab2/list_items.py
@@ -0,0 +1,9 @@
+mylist = ["apple", "banana", "cherry", "mango", "water melon", "kivi"]
+print(mylist[1])
+print(mylist[-1])
+print(mylist[2:5])
+print(mylist[2:])
+print(mylist[:4])
+print(mylist[-4:-2])
+if "apple" in mylist:
+    print("Yes, 'apple' is in the fruits list  ")
