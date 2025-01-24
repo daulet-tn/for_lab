cdbbfd371cd9ffef5ac919a562673e20a8bebba4
daulet_tn <daulettanirberdi94@gmail.com>
2025-01-24 23:06:29 +0500

    ex6

---
+++ addlist.py
@@ -0,0 +1,12 @@
+mylist = ["apple", "banana", "cherry"]
+mylist.append("orenge")
+print(mylist)
+mylist.insert(1, "orenge")
+print(mylist)
+list1 = ["watermelon", "kivi"]
+mylist.extend(list1)
+print(mylist)
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
