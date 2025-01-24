d012348be2faf08488feff39b1bcaf31a2174902
daulet_tn <daulettanirberdi94@gmail.com>
2025-01-24 20:33:53 +0500

    ex4

---
+++ lists.py
@@ -0,0 +1,14 @@
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+print(len(mylist))
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+print(list2)
+print(list3)
+list1 = ["banana", 40, False, 3]
+print(list1)
+print(type(mylist))
+list4 = (("banana", "cherry", "apple"))
+print(list4)
