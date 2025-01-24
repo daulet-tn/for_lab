813fb5119a249bd06e2339fbf4f865f0e9ab7a79
daulet_tn <daulettanirberdi94@gmail.com>
2025-01-24 18:53:01 +0500

    ex2

---
+++ lab2/operators.py
@@ -0,0 +1,33 @@
+a = 9
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a == b)
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+print(a != b)
+print(a > b and a == b)
+print(a > b or a == b)
+print(not a ==b)
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print(x is z)
+print(x is y)
+print(x == y)
+print(x is not z)
+print(x is not y)
+print(x != y)
+print("banana" in x)
+print("pineapple" not in x)
+print(6 & 3)
+print(6 | 3)
+print(6 ^ 3)
+print(~3)
+print(3 << 2)
+print(8 >> 2)
+
