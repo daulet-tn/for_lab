405278dabdca96b5958334eeaabea012ecd84ba4
daulet_tn <daulettanirberdi94@gmail.com>
2025-01-24 22:58:34 +0500

    ex5

---
+++ lab2/change_list.py
@@ -0,0 +1,11 @@
+mylist = ["apple", "banana", "cherry", "mango", "watermelon", "kivi"]
+mylist[1] = "melon"
+print(mylist)
+mylist[2:4] = ["tomato", "carrot"]
+print(mylist)
+mylist[1:2] = ["patato", "kivi"]
+print(mylist)
+mylist[1:3] = ["watermelon"]
+print(mylist)
+mylist.insert(1, "orenge")
+print(mylist)
