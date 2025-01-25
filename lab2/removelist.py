051dd3f0915efb47b53d55440a9a3066b74b91e6
daulet_tn <daulettanirberdi94@gmail.com>
2025-01-25 14:16:01 +0500

    ex7

---
+++ lab2/removelist.py
@@ -0,0 +1,9 @@
+mylist = ["apple", "banana", "cherry", "kivi", "banana", "melon"]
+mylist.remove("banana")
+print(mylist)
+mylist.pop(2)
+print(mylist)
+del mylist[0]
+print(mylist)
+mylist.clear()
+print(mylist)
