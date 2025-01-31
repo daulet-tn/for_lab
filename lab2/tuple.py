c98c3d8df6ba4e10d4de571c6525b06ff4c82e26
daulet_tn <daulettanirberdi94@gmail.com>
2025-01-31 23:02:02 +0500

    1

---
+++ lab2/tuple.py
@@ -0,0 +1,25 @@
+#1
+mytuple = ("apple", "banana", "cherry")
+print(mytuple)
+#2
+mytuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(mytuple)
+#3
+print(len(mytuple))
+#4
+t1 = ("apple",)
+t2 = ("apple")
+print(type(t1))
+print(type(t2))
+#5
+a1 = ("apple", "banana", "kivi")
+a2 = (1, 2, 3, 4, 5)
+a3 = (True, False, True)
+print(a1)
+print(a2)
+print(a3)
+#6
+x1 = (True, 32, "apple")
+print(x1)
+#7
+print(type(a1))
