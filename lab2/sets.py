25a53ec977f05aaf332eaf200882d7e9117746e2
daulet_tn <daulettanirberdi94@gmail.com>
2025-02-05 00:42:04 +0500

    1

---
+++ lab2/sets.py
@@ -0,0 +1,33 @@
+#1
+a = {"apple", "banana", "cherry"}
+print(a)
+#2
+a = {"apple", "banana", "cherry","apple"}
+print(a)
+#3
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+print(thisset)
+#4
+thisset = {"apple", "banana", "cherry", False, True, 0}
+print(thisset)
+#5
+print(len(a))
+#6
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+print(set1)
+print(set2)
+print(set3)
+#7
+set1 = {"abc", 34, True, 40, "male"}
+
+print(set1)
+#8
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+#9
+thisset = set(("apple", "banana", "cherry"))
+print(thisset)
+#10
