564f906c16835ec70a7559385f118c80388d5873
daulet_tn <daulettanirberdi94@gmail.com>
2025-02-04 18:14:32 +0500

    1

---
+++ lab2/accestuple.py
@@ -0,0 +1,15 @@
+#1
+a1 = ("apple", "banana", "cherry")
+print(a1[1])
+#2
+a2 = ("apple", "banana", "cherry", "melon", "watermelon", "kivi", "limon")
+print(a2[2: 5])
+#3
+print(a2[:4])
+#4
+print(a2[2:])
+#5
+print(a2[-4: -2])
+#6
+if "apple" in a2:
+    print("Yes")
