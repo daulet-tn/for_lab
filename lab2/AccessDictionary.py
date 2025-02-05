9fe07302eed445c00ee614ea39b781d4394340ab
daulet_tn <daulettanirberdi94@gmail.com>
2025-02-05 10:16:24 +0500

    1

--- lab2/AccessDictionary.py
+++ lab2/AccessDictionary.py
@@ -101,3 +101,31 @@ car["year"] = 2020
 
 print(x) #after the change
 #10
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.items()
+
+print(x) #before the change
+
+car["color"] = "red"
+
+print(x) #after the change
+#11
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+
+x = car.items()
+
+print(x) #before the change
+
+car["color"] = "red"
+
+print(x) #after the change
+#12
