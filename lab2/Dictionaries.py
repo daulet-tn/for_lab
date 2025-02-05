11f03e8e8c0964476b56cb050e709dc580f7c15d
daulet_tn <daulettanirberdi94@gmail.com>
2025-02-05 09:45:21 +0500

    1

---
+++ lab2/Dictionaries.py
@@ -0,0 +1,39 @@
+#1
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+#2
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+#3
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+#4
+print(len(thisdict))
+#5
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+#6
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+#7
