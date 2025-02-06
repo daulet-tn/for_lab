b54731c18bc584c0fc3603f7fdc9855324781e95
daulet_tn <daulettanirberdi94@gmail.com>
2025-02-06 15:33:14 +0500

    1

---
+++ 123/123.py
@@ -0,0 +1,14 @@
+# user string
+# class str
+# 2 functions: 1. upper, 2. pallindrom
+#  min 4
+x = input()
+class a(txt):
+    def up(txt):
+        print(txt.upper)
+    def pal(txt):
+        z = txt.reverse()
+        if txt == z:
+            print("yes")
+            
+a("dad")
