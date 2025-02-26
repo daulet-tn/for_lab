c78f4439bb306f7d885ba98e514c84caff2a2008
daulet_tn <daulettanirberdi94@gmail.com>
2025-02-26 11:25:15 +0500

    1

--- lab5/regex.py
+++ lab5/regex.py
@@ -21,4 +21,17 @@ print(re.findall(r'[A-Z][a-z]+', text2))
 s = r'a.*b'
 for i in text:
     print(re.fullmatch(s, i))
-
+#6
+text3 = "Hello fifa. start "
+print(re.sub(r'[ ,.]', ':', text3))
+#7
+def upper(snake):
+    return re.sub(r'_(.)', lambda x: x.group(1).upper(), snake)
+print(upper("hello_world_we_start"))
+#8
+text4 = "HelloWorldWeGo"
+print(re.split(r'(?=[A-Z])', text4)[1:])
+#9
+print(re.sub(r'([a-z])([A-Z])', r'\1 \2', text4))
+#10
+print(re.sub(r'([a-z])([A-Z])', r'\1_\2', text4).lower())
