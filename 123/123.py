24e49e2f4c5b8429a286dfd5a9854f7af21c6436
daulet_tn <daulettanirberdi94@gmail.com>
2025-03-25 12:11:05 +0500

    1

--- 123/123.py
+++ 123/123.py
@@ -1,11 +1,3 @@
-# fdsFdfSd - fds_fdf_sd
-# 3+-today
-# 2, 8
-import re
-from datetime import datetime, timedelta 
-x = input()
-s = re.findall(r'_[a-z]', x) 
-z =  len(s) + 1
-a = datetime.now()
-print(a - timedelta(days= z))
-print(a + timedelta(days= z ))
+import pygame
+pygame.init()
+pygame.display.set_mode((800, 600), pygame.OPENGL)
