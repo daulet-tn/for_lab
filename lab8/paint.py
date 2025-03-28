a1c9b81044c2bd7bdfe1c5affa72a72a595962c9
daulet_tn <daulettanirberdi94@gmail.com>
2025-03-28 08:31:17 +0500

    1

--- lab8/paint.py
+++ lab8/paint.py
@@ -4,7 +4,7 @@ import pygame
 pygame.init()
 
 # Настройки окна
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1500, 1000
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Paint in Pygame")
 
